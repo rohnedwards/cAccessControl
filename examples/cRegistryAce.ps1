a9fb1548b9f3c26624a4dabf3c2ba6bb41e38b55
rohnedwards <rohn.edwards@gmail.com>
2015-04-25 22:16:59 -0500

    Added cRegistryAce example

---
+++ examples/cRegistryAce.ps1
@@ -0,0 +1,38 @@
+﻿configuration TestRegistryAceResource {
+    Import-DscResource -Module cAccessControl
+
+    $RegistryPath = "HKLM:\SOFTWARE\ps_dsc_test"
+    Registry DscTestKey {
+        Key = $RegistryPath
+        ValueName = ""
+    }
+
+    cRegistryAce DscTestKeyAccessAce1 {
+        Path = $RegistryPath
+        AceType = "Deny"
+        Principal = "SYSTEM"
+        RegistryRights = "Delete"
+        AppliesTo = "Object, ChildContainers"
+        DependsOn = "[File]DscTestKey"
+    }
+
+    cRegistryAce DscTestKeyAccessAce2 { # This should be inherited
+        Path = $RegistryPath
+        Ensure = "Absent"
+        AceType = "Allow"
+        Principal = "Everyone"
+        RegistryRights = "CreateKey"
+        AppliesTo = "Object"
+        DependsOn = "[File]DscTestKey"
+    }
+
+    cRegistryAce DscTestKeyAuditAce1 {
+        Path = $RegistryPath
+        AceType = "Audit"
+        Principal = "Everyone"
+        RegistryRights = "FullControl"
+        AuditFlags = "Failure"
+        AppliesTo = "Object, ChildContainers"
+        DependsOn = "[File]DscTestFolder"
+    }
+}
